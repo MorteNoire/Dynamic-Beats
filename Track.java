5c002eb7d680f9fca60ce2dcf6c6a71b63091e4b
Rudy Kang <77444457+Rudy-Kang@users.noreply.github.com>
2021-01-15 00:02:52 +0900

    Add files via upload

---
+++ Track.java
@@ -0,0 +1,59 @@
+package dynamic_beat_16;
+
+public class Track {
+
+	private String titleImage; // Title image
+	private String startImage; // The start image before the game starts
+	private String gameImage; //Game image
+	private String startMusic; // The title of the music
+	private String gameMusic; // the music when the game is played
+	private String titleName; // title name of the current song
+	
+	public String getTitleImage() {
+		return titleImage;
+	}
+	public void setTitleImage(String titleImage) {
+		this.titleImage = titleImage;
+	}
+	public String getStartImage() {
+		return startImage;
+	}
+	public void setStartImage(String startImage) {
+		this.startImage = startImage;
+	}
+	public String getGameImage() {
+		return gameImage;
+	}
+	public void setGameImage(String gameImage) {
+		this.gameImage = gameImage;
+	}
+	public String getStartMusic() {
+		return startMusic;
+	}
+	public void setStartMusic(String startMusic) {
+		this.startMusic = startMusic;
+	}
+	public String getGameMusic() {
+		return gameMusic;
+	}
+	public void setGameMusic(String gameMusic) {
+		this.gameMusic = gameMusic;
+	}
+	public String getTitleName() {
+		return titleName;
+	}
+	public void setTitleName(String titleName) {
+		this.titleName = titleName;
+	}
+	
+	public Track(String titleImage, String startImage, String gameImage, String startMusic, String gameMusic, String titleName) {
+		super();
+		this.titleImage = titleImage;
+		this.startImage = startImage;
+		this.gameImage = gameImage;
+		this.startMusic = startMusic;
+		this.gameMusic = gameMusic;
+		this.titleName = titleName;
+	}
+	
+}
